5199b397ab69194a8f0467ece09f0d69c2e9ea69
Prince S. Nayi <naiprince345@gmail.com>
2026-03-30 12:36:24 +0530

    Extra-work

---
+++ C_Lang_Extra-works/A-Z_If-else/A-Z_If-else.c
@@ -0,0 +1,789 @@
+#include<stdio.h>
+
+void main()
+{
+	int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
+	
+	printf("\nEnter the value of A :- ");
+	scanf("%d",&a);
+	printf("\nEnter the value of B :- ");
+	scanf("%d",&b);
+	printf("\nEnter the value of C :- ");
+	scanf("%d",&c);
+	printf("\nEnter the value of D :- ");
+	scanf("%d",&d);
+	printf("\nEnter the value of E :- ");
+	scanf("%d",&e);
+	printf("\nEnter the value of F :- ");
+	scanf("%d",&f);
+	printf("\nEnter the value of G :- ");
+	scanf("%d",&g);
+	printf("\nEnter the value of H :- ");
+	scanf("%d",&h);
+	
+	
+	if(a>b)
+	{
+		if(a>c)
+		{
+			if(a>d)
+			{
+				if(a>e)
+				{
+					if(a>f)
+					{
+						if(a>g)
+						{
+							if(a>h)
+							{
+								printf("\nA is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							if(f>h)
+							{
+								printf("\nF is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+				}
+				else
+				{
+					if(e>f)
+					{
+						if(e>g)
+						{
+							if(e>h)
+							{
+								printf("\nE is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							if(f>h)
+							{
+								printf("\nF is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+				}
+			}
+			else
+			{
+				if(d>e)
+				{
+					if(d>f)
+					{
+						if(d>g)
+						{
+							if(d>h)
+							{
+								printf("\nD is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							if(f>h)
+							{
+								printf("\nF is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+				}
+				else
+				{
+					if(e>f)
+					{
+						if(e>g)
+						{
+							if(e>h)
+							{
+								printf("\nE is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}	
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							if(f>h)
+							{
+								printf("\nF is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			if(c>d)
+			{
+				if(c>e)
+				{
+					if(c>f)
+					{
+						if(c>g)
+						{
+							if(c>h)
+							{
+								printf("\nC is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							if(f>h)
+							{
+								printf("\nF is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+				}
+				else
+				{
+					if(e>f)
+					{
+						if(e>g)
+						{
+							if(e>h)
+							{
+								printf("\nE is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							if(f>h)
+							{
+								printf("\nF is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+				}
+			}
+			else
+			{
+				if(d>e)
+				{
+					if(d>f)
+					{
+						if(d>g)
+						{
+							if(d>h)
+							{
+								printf("\nD is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							if(f>h)
+							{
+								printf("\nF is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+				}
+				else
+				{
+					if(e>f)
+					{
+						if(e>g)
+						{
+							if(e>h)
+							{
+								printf("\nE is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							if(f>h)
+							{
+								printf("\nF is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	else
+	{
+		if(b>c)
+		{
+			if(b>d)
+			{
+				if(b>e)
+				{
+					if(b>f)
+					{
+						if(b>g)
+						{
+							if(b>h)
+							{
+								printf("\nB is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							if(f>h)
+							{
+								printf("\nF is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+				}
+				else
+				{
+					if(e>f)
+					{
+						if(e>g)
+						{
+							if(e>h)
+							{
+								printf("\nE is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							if(f>h)
+							{
+								printf("\nF is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+				}
+			}
+			else
+			{
+				if(d>e)
+				{
+					if(d>f)
+					{
+						if(d>g)
+						{
+							if(d>h)
+							{
+								printf("\nD is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							if(f>h)
+							{
+								printf("\nF is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+				}
+				else
+				{
+					if(e>f)
+					{
+						if(e>g)
+						{
+							if(e>h)
+							{
+								printf("\nE is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+						else
+						{
+							if(g>h)
+							{
+								printf("\nG is biggest...!!");
+							}
+							else
+							{
+								printf("\nH is biggest...!!");
+							}
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							printf("\nF is biggest...!!");
+						}
+						else
+						{
+							printf("\nG is biggest...!!");
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			if(c>d)
+			{
+				if(c>e)
+				{
+					if(c>f)
+					{
+						if(c>g)
+						{
+							printf("\nC is biggest...!!");
+						}
+						else
+						{
+							printf("\nG is biggest...!!");
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							printf("\nF is Biggest...!!");
+						}
+						else
+						{
+							printf("\nG is biggest...!!");
+						}
+					}
+				}
+				else
+				{
+					if(e>f)
+					{
+						if(e>g)
+						{
+							printf("\nE is biggest...!!");
+						}
+						else
+						{
+							printf("\nG is biggest...!!");
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							printf("\nF is biggest...!!");
+						}
+						else
+						{
+							printf("\nG is biggest...!!");
+						}
+					}
+				}
+			}
+			else
+			{
+				if(d>e)
+				{
+					if(d>f)
+					{
+						if(d>g)
+						{
+							printf("\nD is biggest...!!");
+						}
+						else
+						{
+							printf("\nG is biggest...!!");
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							printf("\nF is biggest...!!");
+						}
+						else
+						{
+							printf("\nG is biggest...!!");
+						}
+					}
+				}
+				else
+				{
+					if(e>f)
+					{
+						if(e>g)
+						{
+							printf("\nE is biggest...!!");
+						}
+						else
+						{
+							printf("\nG is biggest...!!");
+						}
+					}
+					else
+					{
+						if(f>g)
+						{
+							printf("\nF is biggest...!!");
+						}
+						else
+						{
+							printf("\nG is biggest...!!");
+						}
+					}
+				}
+			}
+		}
+	}
+}
